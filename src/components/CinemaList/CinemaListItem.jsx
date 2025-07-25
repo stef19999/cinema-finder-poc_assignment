5e58828ce4383d158ad7c35e8dc29f220e5027a5
stef19999 <103026077+stef19999@users.noreply.github.com>
2025-07-25 12:37:37 +1200

    Initial commit

---
+++ src/components/CinemaList/CinemaListItem.jsx
@@ -0,0 +1,51 @@
+import { Chip, IconButton, ListItem, ListItemText } from "@mui/material";
+import { MdCall, MdOutlineLocationOn } from "react-icons/md";
+import { format } from "d3-format";
+import Tooltip from "@mui/material/Tooltip";
+
+const dispatchMapSnapTo = (lat, lng) => {
+  // This will dispatch the `map.snapTo` event which will trigger a listener on the
+  // respective active map component to zoom to the latitude and longitude passed
+  console.log(
+    "triggering `map.snapTo` event with args: ",
+    `lat: ${lat}, lng: ${lng}`
+  );
+  dispatchEvent(new CustomEvent("map.snapTo", { detail: { lat, lng } }));
+};
+
+const CinemaListItem = ({
+  name,
+  lat,
+  lng,
+  phoneNumber,
+  distance,
+  ...otherProps
+}) => {
+  return (
+    <ListItem>
+      <ListItemText>
+        {name}
+        {distance && (
+          <Chip
+            size="small"
+            sx={{ ml: 1 }}
+            label={`${format(",.1f")(distance)} km`}
+          />
+        )}
+      </ListItemText>
+      {phoneNumber && (
+        <Tooltip title="Call this cinema">
+          <IconButton component="a" href={`tel:${phoneNumber}`}>
+            <MdCall />
+          </IconButton>
+        </Tooltip>
+      )}
+      <Tooltip title="Locate on Map">
+        <IconButton onClick={() => dispatchMapSnapTo(lat, lng)}>
+          <MdOutlineLocationOn />
+        </IconButton>
+      </Tooltip>
+    </ListItem>
+  );
+};
+export default CinemaListItem;
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,31 @@
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import SiteLogo from "./SiteLogo";
+import MapTypeMenu from "./MapTypeMenu";
+
+const TopBar = () => (
+  <AppBar position="static">
+    <Container maxWidth="xl">
+      <Toolbar disableGutters>
+        <SiteLogo />
+        <Typography
+          variant="h6"
+          noWrap
+          component={RouterLink}
+          to="/"
+          sx={{
+            fontFamily: "monospace",
+            fontWeight: 700,
+            color: "inherit",
+            textDecoration: "none",
+            cursor: "default",
+          }}
+        >
+          Cinema Finder
+        </Typography>
+        <MapTypeMenu />
+      </Toolbar>
+    </Container>
+  </AppBar>
+);
+export default TopBar;
